1ddd07eb22d73f8137bab64c5f5eda3ef287565c
googlegeminilist-crypto <googlegemini.list@tj.mell.net>
2026-02-22 21:15:15 +0000

    better

---
+++ Hidden Notes/Hidden NotesTests/Hidden_NotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Hidden_NotesTests.swift
+//  Hidden NotesTests
+//
+//  Created by Thomas Mellor on 22/02/2026.
+//
+
+import Testing
+@testable import Hidden_Notes
+
+struct Hidden_NotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
